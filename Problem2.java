b1f49555740944de897c4885f4c8eff0d3fc7849
Shrek-with-no-r <cmsarr100@student.hbuhsd.edu>
2024-09-13 20:42:18 -0700

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -2,7 +2,12 @@ public class Problem2
 {
     public static void main(String[] Args)
     {
-        
+        int num1 = 6;
+        int num2 = 7;
+        int num3 = 8;
+        System.out.println("First digit: " + (num1 %= 10) );
+        System.out.println("Second digit: " + (num2 %= 100) );
+        System.out.println("Third digit: " + (num3 %= 1000) );
     }
 }
 
